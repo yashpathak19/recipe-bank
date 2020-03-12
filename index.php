8b4e4dad6dc0067bc9731eb003d379492cf5ee5d
shahkrishna1295 <shahkrishna1295@gmail.com>
2020-03-12 14:27:13 -0400

    Initial commit| Added header footer

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Home Page</title>
+        <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <header>
+            <?php include 'header.php'?>
+        </header>
+
+        <main>
+            
+        </main>
+
+        <footer class="page-footer font-small ">
+            <?php include 'footer.php'?>
+        </footer>
+    </body>
+</html>
---
+++ sitemap.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Site Map</title>
+        <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <?php include 'header.php'?>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <ul>
+                        <li><a href="index.php">Recipie Bank</a></li>
+                        <ul>
+                            <li><a href="login.php">Login</a></li>
+                            <li><a href="signup.php">Sign Up</a></li>
+                            <li><a href="index.php">Feed</a></li>
+                            <li><a href="#">Starters</a></li>
+                            <li><a href="#">Main Course</a></li>
+                            <li><a href="#">Desserts</a></li>
+                            <li><a href="#">Drinks</a></li>
+                            <li><a href="#">Newsletter</a></li>
+                            <li><a href="#">SiteMap</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                        <li><a href="#">Feed</a></li>
+                        <li><a href="#">Starters</a></li>
+                        <li><a href="#">Main Course</a></li>
+                        <li><a href="#">Desserts</a></li>
+                        <li><a href="#">Drinks</a></li>
+                        <li><a href="#">Newsletter</a></li>
+                        <li><a href="#">SiteMap</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="page-footer font-small ">
+        <?php include 'footer.php'?>
+    </footer>
+    </body>
+</html>
